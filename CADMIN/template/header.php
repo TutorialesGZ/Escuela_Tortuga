b4f4b107f744145119bdd5e052822c0e742c60ae
H4ShiS <116536015+H4ShiS@users.noreply.github.com>
2022-10-31 19:17:06 -0600

    Add files via upload

---
+++ CADMIN/template/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="es">
+  <head>
+    <title>CRUD Docentes</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.0.2 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"  integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- cdn icnonos-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+  </head>
+  <body style="background: black;" >
+      <div class="container-fluid bg-warning">
+          <div class="row" style="background-color: #8D2613; ">
+              <div class="col-md">
+                  <header class="py-3">
+                      <h3 class="text-center" style="color: #FFFFFF"> Escuela Tortuga <a href="http://localhost/EscuelaTortuga/pages/Home.php"> <input type="submit" class="btn btn-light" value="Regresar"> </a></h3>
+                  </header>
+              </div>
+          </div>
+      </div>
+
+
